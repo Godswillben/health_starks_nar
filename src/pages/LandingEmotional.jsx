277f112acf36fb746fe2ff46c1dbcc321b6ffe8d
Godswillben <amogugodswill99@gmail.com>
2026-06-07 20:50:51 +0100

    Normal narrative based UI/UX

---
+++ src/pages/LandingEmotional.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import LandingHeader from '../components/landing/LandingHeader';
+import LandingBanner from '../components/landing/LandingBanner';
+import HowItWorks from '../components/landing/HowItWorks';
+
+const LandingEmotional = () => {
+    return (
+    <>
+      <LandingHeader />
+      <LandingBanner />
+      <HowItWorks />
+    </>
+    );
+}
+
+export default LandingEmotional;
