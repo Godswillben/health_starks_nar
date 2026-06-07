fd23badbfee6b2d634b9464f5640c40ba004badd
Godswillben <amogugodswill99@gmail.com>
2026-06-07 20:50:20 +0100

    Landing page for trend demo

---
+++ src/pages/LandingContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingHeader from '../components/landing/LandingHeader';
+import LandingContentBanner from '../components/landing_con/LandingContentBanner';
+
+const LandingContent = () => {
+    return (
+        <div>
+            <LandingHeader />
+            <LandingContentBanner />
+        </div>
+    );
+}
+
+export default LandingContent;
