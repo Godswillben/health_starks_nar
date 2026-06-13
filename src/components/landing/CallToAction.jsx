acf835accdd8509928205635718580f757902438
Godswillben <amogugodswill99@gmail.com>
2026-06-13 19:09:07 +0100

    Landing page call to action components responsive

--- src/components/landing/CallToAction.jsx
+++ src/components/landing/CallToAction.jsx
@@ -4,7 +4,7 @@ import arrowIcon from "../../assets/landing/Arrow.svg";
 const CallToAction = ({cls="w-9/11 ", text=""}) => {
    return (
       <div className={`flex items-center gap-2 cursor-pointer group border-b border-blue500 p-1 ${cls}`}>
-         <p className="text-blue500 font-medium group-hover:underline font-Marck text-xl tracking-wide">
+         <p className="text-blue500 font-medium group-hover:underline font-Marck text-xl sm:text-2xl tracking-wide sm:tracking-wider">
            {text}
          </p>
          <img
