1dcc4ed429d68c731469f9122cbc7999977a1e37
Godswillben <amogugodswill99@gmail.com>
2026-06-13 19:11:59 +0100

    Landing page whusInfobox  components responsive

--- src/components/landing/WhyUsInfoBox.jsx
+++ src/components/landing/WhyUsInfoBox.jsx
@@ -4,7 +4,7 @@ const WhyUsInfoBox = ({text}) => {
    return (
       <div className="flex space-x-4 items-center">
          <div className="size-2 bg-blue800 rounded-full"></div>
-         <p className="font-Comm text-blue800 flex-1">
+         <p className="font-Comm text-blue800 sm:text-lg md:text-xl flex-1">
             {text}
          </p>
       </div>
