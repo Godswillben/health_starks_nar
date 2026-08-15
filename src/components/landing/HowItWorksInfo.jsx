1a1cd1a482e3d90b0f4e63017f6c3740e595c33b
Godswillben <amogugodswill99@gmail.com>
2026-08-15 09:30:02 +0100

    landing page component sections

--- src/components/landing/HowItWorksInfo.jsx
+++ src/components/landing/HowItWorksInfo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const HowItWorksInfo = ({head, body, cls="mb-17"}) => {
+const HowItWorksInfo = ({head, body, cls="mb-23 sm:mb-4"}) => {
    return (
-      <div className={`flex flex-col space-y-2 ${cls}`}>
+      <div className={`flex flex-col space-y-4 ${cls} sm:w-5/11`}>
          <h4 className="text-brown700 text-2xl font-Peralta tracking-wider">{head}</h4>
          <p className="font-Comm text-xl text-blue800 font-semibold">
             {body}
--- src/components/landing/LandingAboveRash.jsx
+++ src/components/landing/LandingAboveRash.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import phone from "../../assets/landing/phone.svg";
 
-const LandingAboveRash = ({img, pos=" -right-10 top-50", imgcls="w-50"}) => {
+const LandingAboveRash = ({img, pos=" -right-10 top-50 sm:top-35 md:right-25 lg:right-82 xl:right-32", imgcls="w-50 sm:w-70"}) => {
    return (
       <div className={`absolute z-20 ${pos}`}>
          <img className={`${imgcls}`} src={img ? img : phone} alt="" />
